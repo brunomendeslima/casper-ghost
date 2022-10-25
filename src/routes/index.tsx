0daf1b1ee197ae83e97bf920552dde07a45794a1
Bruno Mendes Lima <brunolima.qi@gmail.com>
2022-10-25 20:45:32 -0300

    configuração firebase google auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import Home from "../pages/home"
 
 export const AppRoutes = () => {
 
     return(
         <Routes>
-            <Route path="/" element={<p><b>Eu te amo! S2</b></p>} />
+            <Route path="/" element={<Home />} />
             <Route path="/2" element={<p>Página 2</p>} />
             <Route path="/3" element={<p>Página 3</p>} />
             <Route path="*" element={<Navigate to="/" />} />
